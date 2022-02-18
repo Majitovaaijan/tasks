adc700d62243c4616e581bac976d8c6abf4aeaac
Aijan <aijanka.9239@gmail.com>
2022-02-18 18:42:51 +0600

    feat: added task

---
+++ 18.02/1/script.js
@@ -0,0 +1,40 @@
+/**********#1***********/
+
+let num = 10;
+num = num++;
+num = ++num;
+num = num--;
+console.log(num);
+
+/**********#2***********/
+
+let name = 'Айжан';
+alert(`Привет, ${name}!`);
+
+
+/**********#3***********/
+
+let age = 29;
+alert(`Мне ${age} лет!`);
+
+
+/**********#4***********/
+
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result);
+
+
+/**********#5***********/
+
+let num1 = 47;
+num1 += 7;
+num1 -= 18;
+num1 *= 10;
+num1 /= 15;
+alert(num1);
+
+
