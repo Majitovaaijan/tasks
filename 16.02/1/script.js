36d37fff563a150ffc8a4f362bc8700cd19a55cc
Aijan <aijanka.9239@gmail.com>
2022-02-18 18:17:13 +0600

    feat: added task

---
+++ 16.02/1/script.js
@@ -0,0 +1,16 @@
+let firstNumber = 1;
+let second_number = 2;
+let ThirdNumber = 3;
+
+alert(firstNumber);
+alert(second_number);
+alert(ThirdNumber);
+
+let str1 = 'Салам,';
+let str2 = 'Бишкек!';
+let str3 = `${str1} ${str2}`;
+
+alert(str3);
+
+
+// alert(str1 + ' ' + str2)
