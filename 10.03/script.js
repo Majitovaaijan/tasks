dd7318ed5c15de621189e1fe52456a4d252f1f31
Aijan <aijanka.9239@gmail.com>
2022-03-10 11:31:35 +0600

    init

---
+++ 10.03/script.js
@@ -0,0 +1,10 @@
+let btn1 = document.querySelector('#start');
+let btn2 = document.querySelector('#stop');
+let btn3 = document.querySelector('#restart');
+
+let time1 = document.querySelector('#hour');
+let time2 = document.querySelector('#minute');
+let time3 = document.querySelector('#second');
+
+
+let count = 0;
